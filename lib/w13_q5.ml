00a5dffbf32a0b94658081aa44142bea87d89702
xinyuluo <luoxinyu06@gmail.com>
2019-04-13 21:57:19 +0800

    Update w13_q5.ml

--- lib/w13_q5.ml
+++ lib/w13_q5.ml
@@ -1,13 +1,12 @@
-open Core_kernel
-open Week_12_Graphs
 open Week_13_Reachability
 open Week_12_BST
 open BinarySearchTree
 open Week_11_UnionFind
+open Week_12_Graphs
 open LinkedGraphs
+open UnionFind
 
 let random_spanning_tree_finder g =
-  let open UnionFind in
   let forest = mk_UF (v_size g) in
   let tree = ref [] in
   let edges = Set.elements g.edges in
@@ -15,12 +14,12 @@ let random_spanning_tree_finder g =
     let nd = List.map (fun c -> (Random.bits (), c)) l in
     let sond = List.sort compare nd in
     List.map snd sond in
-  List.iter (shuffle edges) ~f:(fun (u, v) ->
+  List.iter (fun (u, v) ->
       let su = find forest u in
       let sv = find forest v in
       if su <> sv
       then begin
         tree := (u, v) :: !tree;
         union forest u v
-      end);
+      end) (shuffle edges);
   !tree
