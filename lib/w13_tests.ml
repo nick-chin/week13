5fefe195b45dcf1c5e1c53045af7a5976fdbebb0
xinyuluo <luoxinyu06@gmail.com>
2019-04-14 18:28:50 +0800

    Update w13_tests.ml

--- lib/w13_tests.ml
+++ lib/w13_tests.ml
@@ -6,54 +6,13 @@ let%test "Random Knight Path" =
 
 
 open W13_q5
-open Week_11_UnionFind
-open UnionFind
-open Week_12_Graphs
-open LinkedGraphs
-open Week_13_Spanning
-open Week_13_Reachability
-open Week_13_Paths
-  
-let%test "Testing Spanning Tree Size" = 
-  let t = random_spanning_tree_finder example_graph_undirected in 
-  List.length t = v_size example_graph_undirected - 1
 
-let%test "Testing Spanning Tree Cycle" = 
+let%test "Testing Random Spanning Tree" =
   let g = example_graph_undirected in
-  let tree = random_spanning_tree_finder g in
-  let forest = mk_UF (v_size g) in
-  let rec check_cycle e =
-  match e with
-  | [] -> true
-  | [(u,v)] -> (find forest u) <> (find forest v)
-  | (u,v) :: t -> 
-    let su = find forest u in
-    let sv = find forest v in
-    if su <> sv then (union forest u v; check_cycle t) else false in
-check_cycle tree
-
-let%test "Testing Spanning Tree Connectness" = 
-  let g = example_graph_undirected in
-  let t = random_spanning_tree_finder g in
-  match t with
-  | [] -> true
-  | _ ->
-  let t_graph = mk_graph (v_size g) in
-  let nodes = Set.elements g.nodes in
-  List.iter (fun n -> add_node t_graph n) nodes;
-  List.iter (fun (a,b) -> add_edge t_graph a b) t;
-  List.iter (fun (a,b) -> add_edge t_graph b a) t;
-  List.for_all (fun n -> is_reachable t_graph (List.hd nodes) n) nodes
-
-let%test "Testing Spanning Tree Weight" = 
-  let g = example_graph_undirected in
-  let rst = random_spanning_tree_finder g in
-  let mst = mst_kruskal g in
-  let rst_weight = List.map (fun (a,b) -> get_linked_edge_label g a b) rst in
-  let mst_weight = List.map (fun (a,b) -> get_linked_edge_label g a b) mst in
-  let rec sum wl =
-    match wl with
-    | [] -> 0
-    | h :: t -> h + (sum t)
-  in
-  (sum rst_weight) >= (sum mst_weight)
+  let n = ref 5 in
+  let counter = ref true in
+  while !n <> 0 do
+    counter := !counter && test_rst_size g && test_rst_connectness g && test_rst_connectness g && test_compare_weight_rst_mst g;
+    n := !n - 1;
+  done;
+  !counter
