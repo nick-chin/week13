d6d37444a1724486b7e8579880f9ad6b7b50e7b4
anp.ync <40881096+alaukiknpant@users.noreply.github.com>
2019-04-14 19:14:34 +0800

    Update BFS.ml

--- lib/BFS.ml
+++ lib/BFS.ml
@@ -1,7 +1,7 @@
 open Week_01
 open Week_06
 open Week_13_Paths
-open Core_kernel
+
 open Week_12_Graphs
 open Week_13_Reachability
 open Week_12_BST
@@ -15,7 +15,11 @@ module GraphBFS = struct
 
   type color = White | Gray | Black
 
-  let bfs g s =
+  let  bfs g =
+
+
+
+
     let color_map = mk_new_table (v_size g) in
     let parent_map = mk_new_table (v_size g) in
     let tree_map = mk_new_table (v_size g) in
@@ -25,13 +29,15 @@ module GraphBFS = struct
     let all_nodes = get_nodes g in
 
     (* Make all nodes white *)
-    List.iter all_nodes (fun n -> insert color_map n White) ;
+    List.iter  (fun n -> insert color_map n White) all_nodes;
     (* Make all distances Infinity *)
-    List.iter all_nodes (fun n -> insert distance_map n Infinity) ;
+    List.iter  (fun n -> insert distance_map n Infinity) all_nodes ;
     (*Make the parent of each node None*)
-    List.iter all_nodes (fun n -> insert parent_map n None) ;
+    List.iter  (fun n -> insert parent_map n None) all_nodes ;
+    (*Initialize tree map*)
+    List.iter (fun n -> insert tree_map n []) all_nodes;
 
-
+     let helper g s =
     (*Make color of s gray*)
     insert color_map s Gray;
     (*Make distance of s Finite 0)*)
@@ -41,20 +47,19 @@ module GraphBFS = struct
 
 
     (* Make a queue *)
-    let q = mk_queue (v_size g);
+    let q = mk_queue (v_size g) in
     (*Enuqueue (q,s)*)
     enqueue q s;
 
 
-    while not (is_empty q) do (
+    while not (is_empty q) do
      let u = get_exn (dequeue q) in
-     let lst = get_succ g u in
 
 (*Perform action on all adjacent vertices*)
 
 (*Whenever a white vertex is found while scanning for u, the vertex v
 and the edge (u, v) are added to the tree.*)
-    lst |> List.iter (fun v ->
+    get_succ g u |> List.iter (fun v ->
         let v_color = get_exn @@ get color_map v in
         if v_color = White
         then begin
@@ -64,24 +69,24 @@ and the edge (u, v) are added to the tree.*)
           let siblings = get_exn @@ get tree_map u in
           insert tree_map u (v :: siblings);
           enqueue q v;
-          end;
-    else if v_color = Gray
-        then has_cycles := true;
-    else if v_color = Black
-        then has_cycles := true)
-      insert color_map u Black; 
-        done)
+          end
+        else if v_color = Gray
+        then has_cycles := true
+        else if v_color = Black
+        then has_cycles := true);
+      insert color_map u Black;
+   done in
 
 
 (* We iterate through all the nodes with white color*)
 
-List.iter (fun n ->
-      if get_exn @@ get color_map n = White
-      then begin
-        (* Record roots *)
-        roots := n :: !roots;
-        bfs g n
-      end)
-    all_nodes;
-    (!roots, parent_map, distance_map, tree_map);
-end
+      all_nodes |> List.iter (fun n ->
+        if get_exn @@ get color_map n = White
+        then begin
+          (* Record roots *)
+          roots := n :: !roots;
+          helper g n
+        end);
+    (!roots, parent_map, distance_map, tree_map)
+
+end;;
