bf3b0d6e2cbcbe65b07302cb51bc9ad5f103cfa0
Linfan XIAO (Linda) <linfanxiaolinda@outlook.com>
2019-04-17 10:49:30 +0800

    reverting to original

--- lib/w13_q3.ml
+++ lib/w13_q3.ml
@@ -23,99 +23,91 @@ let rec get_path_weight g path = match path with
     w + get_path_weight g t
   | _ -> 0
 
-(* sort a list by another list *)
-let sort_l1_by_l2 l1 l2 =
-  let zipped = list_zip l1 l2 in
-  let comp x y =
-    if (snd x < snd y) then (-1)
-    else if (snd x > snd y) then 1
-    else 0
-  in
-  let sorted = List.sort comp zipped in
-  let l1_sorted = List.map (fun e -> fst e) sorted in
-  l1_sorted
+(* relax edge (u, v) in increasing order *)
+let relax_increasing dist_table prev_tree w u v =
+  let open Distance in
+  (* distance from s to v through u *)
+  let vud = dist dist_table u + (Finite (w u v)) in
+  let pred_u = get prev_tree u in
+  (* update if and only if going from s to v through u is shorter and the distance from u to v is longer than the incoming edge to u in the current path *)
+  if (dist dist_table v > vud) && ((pred_u <> None && Finite (w u v) > Finite (w (get_exn pred_u) u)) || (pred_u = None))
+  then
+    begin
+      NodeTable.insert dist_table v vud;
+      NodeTable.insert prev_tree v u
+    end
 
-(* find increasing shortest path from node init to node final *)
-let increasing_shortest_path g init final =
-  let w = get_linked_edge_label g in
-  let rec walk path visited n = 
-    if n = final
-    then Some path
-    else if List.mem n visited 
-    then None
-    else
-      let successors =  get_succ g n in
-      let weights = List.map (fun e -> w n e) successors in
-      (* sort successors by weight *)
-      let successors = sort_l1_by_l2 successors weights in
-      let visited' = n :: visited in
-      let rec iter = function 
-        | [] -> None 
-        | h :: t ->
-            if path = [] || (w n h) > w (fst (List.hd path)) (snd (List.hd path))
-            then
-              let path' = (n, h) :: path in
-              match walk path' visited' h with
-              | None -> iter t
-              | Some p -> Some p
-            else
-              iter t
-      in iter successors
-  in
-  match walk [] [] init with
-  | Some p -> Some (List.rev p)
-  | _ -> None
+(* relax edge (u, v) in decreasing order *)
+let relax_decreasing dist_table prev_tree w u v =
+  let open Distance in
+  (* distance from s to v through u *)
+  let vud = dist dist_table u + (Finite (w u v)) in
+  let pred_u = get prev_tree u in
+  (* update if and only if going from s to v through u is shorter and the distance from u to v is shorter than the incoming edge to u in the current path *)
+  if (dist dist_table v > vud) && ((pred_u <> None && Finite (w u v) < Finite (w (get_exn pred_u) u)) || (pred_u = None))
+  then
+    begin
+      NodeTable.insert dist_table v vud;
+      NodeTable.insert prev_tree v u
+    end
 
-(* find decreasing shortest path from node init to node final *)
-let decreasing_shortest_path g init final =
-  let w = get_linked_edge_label g in
-  let rec walk path visited n = 
-    if n = final
-    then Some path
-    else if List.mem n visited 
-    then None
-    else
-      let successors =  get_succ g n in
-      let weights = List.map (fun e -> w n e) successors in
-      (* sort successors by weight *)
-      let successors = sort_l1_by_l2 successors weights in
-      let visited' = n :: visited in
-      let rec iter = function 
-        | [] -> None 
-        | h :: t ->
-            if path = [] || (w n h) < w (fst (List.hd path)) (snd (List.hd path))
-            then
-              let path' = (n, h) :: path in
-              match walk path' visited' h with
-              | None -> iter t
-              | Some p -> Some p
-            else
-              iter t
-      in iter successors
+(* find increasing shortest path from node s to every other node in g *)
+let increasing_shortest_path g s =
+  let open Distance in
+  let (w, d, p) = initialise_single_source g s in
+  let all_edges = elements g.edges in
+  for i = 0 to v_size g - 1 do
+    List.iter (fun (u, v) -> relax_increasing d p w u v) all_edges
+  done;
+  let rec check_neg_cycles es =
+    match es with
+    | [] -> true
+    | (u, v) :: t ->
+      if dist d v > dist d u + (Finite (w u v))
+      then false
+      else check_neg_cycles t
   in
-  match walk [] [] init with
-  | Some p -> Some (List.rev p)
-  | _ -> None
+  ((p, d), check_neg_cycles all_edges)
 
-(* find monotonic shortest path from node s to node t *)
+(* find decreasing shortest path from node s to every other node in g *)
+let decreasing_shortest_path g s =
+  let open Distance in
+  let (w, d, p) = initialise_single_source g s in
+  let all_edges = elements g.edges in
+  for i = 0 to v_size g - 1 do
+    List.iter (fun (u, v) -> relax_decreasing d p w u v) all_edges
+  done;
+  let rec check_neg_cycles es =
+    match es with
+    | [] -> true
+    | (u, v) :: t ->
+      if dist d v > dist d u + (Finite (w u v))
+      then false
+      else check_neg_cycles t
+  in
+  ((p, d), check_neg_cycles all_edges)
+
+(* get monotonic shortest path from s to t in g *)
 let monotonic_shortest_path g s t =
-  let increasing_path = increasing_shortest_path g s t in
-  let decreasing_path = decreasing_shortest_path g s t in
+  let ((increasing_p, increasing_d), increasing_bool) = increasing_shortest_path g s in
+  let ((decreasing_p, decreasing_d), decreasing_bool)  = decreasing_shortest_path g s in
+  let increasing_path = get_shortest_path increasing_p s t in
+  let decreasing_path = get_shortest_path decreasing_p s t in
   match (increasing_path, decreasing_path) with
   | (None, None) -> None
   | (None, Some path) ->
-      Some path
+     Some path
   | (Some path, None) ->
-      Some path
+     Some path
   | (Some path1, Some path2) ->
-      let weight1 = get_path_weight g path1 in
-      let weight2 = get_path_weight g path2 in
-      if weight1 < weight2
-      then Some path1
-      else if weight1 > weight2
-      then Some path2
-      else Some path1;;
-
+     let weight1 = get_path_weight g path1 in
+     let weight2 = get_path_weight g path2 in
+     if weight1 < weight2
+     then Some path1
+     else if weight1 > weight2
+     then Some path2
+     else Some path1
+     
 (*
 let g = example_graph_bf
 monotonic_shortest_path g 0 1
@@ -295,76 +287,7 @@ let tester_monotonic _ =
   test_monotonic_shortest_path example_graph_bf &&
     test_monotonic_shortest_path example_graph_dijkstra
 
-(*****************************************)
-(*             Random tests              *)
-(*****************************************)
-
-(* generate random graphs *)
-let random_nodes vNum =
-  Array.init vNum (fun i -> Char.escaped @@ char_of_int ((Random.int 26) + 97))
-
-let remove_duplicate l =
-  let comp x y =
-    if fst x < fst y then -1
-    else if fst x > fst y then 1
-    else
-      if snd x < snd y then -1
-      else if snd x > snd y then 1
-      else 0
-  in
-  let sorted = List.sort comp l in
-  let rec walk l' acc =
-    match l' with
-    | h1 :: h2 :: t ->
-       if h1 = h2 then walk t (h1 :: acc)
-       else walk t (h2 :: h1 :: acc)
-    | [h] -> h :: acc
-    | _ -> acc
-  in
-  walk sorted []
-
-(* generate random edges for a graph with vNum vertices and eNum edges *)
-let random_edges vNum eNum =
-  let es' = List.map (fun i -> (Random.int vNum, Random.int vNum)) (iota (eNum - 1)) in
-  let es = List.filter (fun (x, y) -> x <> y) es' in
-  (* remove duplicate edges *)
-  remove_duplicate es
-
-(* generate random weights for a list of edges *)
-let random_weights es =
-  let weights = List.map (fun i -> Random.int 10) es in
-  weights
-
-(* generate random labels *)
-let random_labels es =
-  let weights = random_weights es in
-  let zipped = list_zip es weights in
-  let labels = List.map (fun e -> (fst (fst e), snd (fst e), snd e)) zipped in
-  labels
-
-(* generate random directed graphs *)
-let random_digraph vNum eNum =
-  let nodes = random_nodes vNum in
-  let es = random_edges vNum eNum in
-  let labels = random_labels es in
-  read_graph_and_payloads vNum nodes es labels
-
-(* tests on 20 random graphs *)
-let test_random_monotonic_shortest_path vNum eNum =
-  let results = ref [] in
-  for _ = 1 to 20 do
-    let g = random_digraph vNum eNum in
-    let res = test_monotonic_shortest_path g in
-    results := res :: !results
-  done;
-  List.for_all (fun e -> e)  !results
-
-(* random tester *)
-let tester_monotonic_random _ =
-  test_random_monotonic_shortest_path 5 20
-
 (*
 open W13_q3
 let%test "monotonic_shortest_path" = tester_monotonic ()
-let%test "monotonic_shortest_path_random" = tester_monotonic_random ()
 *)
